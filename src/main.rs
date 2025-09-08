c526de9f7e7ac15ac6fe6e0ba62ab2f0053e062d
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-09-08 10:06:33 +0200

    Fix logger being init() twice

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ use log::info;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
-    env_logger::init();
 
     let pkg_name = env!("CARGO_PKG_NAME");
     let pkg_version = env!("CARGO_PKG_VERSION");
