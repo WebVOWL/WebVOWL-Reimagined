9edbdf0a2c26bf8a6c207a7b13f241a53accf44f
manfred-dahl <manfred@Manfred.localdomain>
2025-09-18 11:30:39 +0200

    start of new GUI

--- src/app.rs
+++ src/app.rs
@@ -6,14 +6,13 @@ use leptos_router::{
     components::{FlatRoutes, Route, Router},
 };
 use thaw::*;
-
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
         <Stylesheet id="webvowl" href="/pkg/webvowl.css" />
-        // <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico" />
+        //<Link rel="shortcut icon" type_="image/ico" href="/favicon.ico" />
         <Router>
             <FlatRoutes fallback=|| "Page not found.">
                 <Route path=StaticSegment("") view=Home />
@@ -27,7 +26,7 @@ fn Home() -> impl IntoView {
     let on_select = move |value: &str| leptos::logging::warn!("{}", value);
     //search bar
     let searchvalue = RwSignal::new(String::new());
-    //filter menu
+    //filter menu    
     let datatypepropertycheck = RwSignal::new(false);
     let objectpropertycheck = RwSignal::new(false);
     let solitarypropertycheck = RwSignal::new(false);
@@ -49,38 +48,34 @@ fn Home() -> impl IntoView {
     let ontologyversion = RwSignal::new("0.99".to_string());
     let ontologyauthors = RwSignal::new("Alice, Bob, Charlie".to_string());
     let ontologylanguages = RwSignal::new(vec!["en".to_string()]);
-
     view! {
         <Title text="Leptos + Tailwindcss" />
         <main>
-            <canvas id="canvas" width=800 height=600 />
             <div class="min-h-screen bg-[rgba(201, 196, 196, 1)]">
-                <div class="flex fixed bottom-0 left-0 flex-row flex-wrap p-0 font-mono text-white">
+                <div class="fixed bottom-0 left-0 flex flex-row flex-wrap p-0 font-mono text-white">
                     <ConfigProvider>
                         <Input placeholder="Search" value=searchvalue />
                         <Button class="locateButton">"⌖"</Button>
-                        // ontology menu
+                        //ontology menu
                         <Menu on_select=on_select position=MenuPosition::Top>
                             <MenuTrigger slot>
                                 <Button icon=icondata::BiMenuRegular>"Ontology"</Button>
                             </MenuTrigger>
-                            <Button>"Friend of a Friend (FOAF) vocabulary"</Button>
+                            <Button>"Friend of a Friend (FOAF) vocabulary"</Button> 
                             <Button>"GoodRelations Vocabulary for E-Commerce"</Button>
-                            <Button>
-                                "Modular and Unified Tagging Ontology (MUTO)"
-                            </Button>
-                            <Button>"Personas Ontology (PersonasOnto)"</Button>
-                            <Button>
-                                "SIOC (Semantically-Interlinked Online Communities) Core Ontology"
-                            </Button>
+                            <Button>"Modular and Unified Tagging Ontology (MUTO)"</Button>
+                            <Button>"Personas Ontology (PersonasOnto)"</Button>  
+                            <Button>"SIOC (Semantically-Interlinked Online Communities) Core Ontology"</Button>
                             <Button>"Benchmark Graph for VOWL"</Button>
                             <Label>"Custom Ontology"</Label>
-                            <Input placeholder="Enter ontology IRI" />
+                            <Input placeholder="Enter ontology IRI"/>
                             <Upload>
-                                <Button>"Select ontology file"</Button>
+                                <Button>
+                                    "Select ontology file"
+                                </Button>
                             </Upload>
                         </Menu>
-                        // export menu
+                        //export menu
                         <Menu on_select position=MenuPosition::Top>
                             <MenuTrigger slot>
                                 <Button icon=icondata::BiExportRegular>"Export"</Button>
@@ -91,79 +86,38 @@ fn Home() -> impl IntoView {
                             <Button>"Export as TTL"</Button>
                             <Button>"Export as URL"</Button>
                         </Menu>
-                        // filter menu
+                        //filter menu
                         <Menu on_select position=MenuPosition::Top>
                             <MenuTrigger slot>
                                 <Button icon=icondata::BiFilterAltRegular>"Filter"</Button>
                             </MenuTrigger>
-                            <Checkbox
-                                checked=datatypepropertycheck
-                                label="Datatype properties"
-                            />
-                            <Checkbox
-                                checked=objectpropertycheck
-                                label="Object properties"
-                            />
-                            <Checkbox
-                                checked=solitarypropertycheck
-                                label="Solitary properties"
-                            />
-                            <Checkbox
-                                checked=classdisjointnesscheck
-                                label="Class disjointness"
-                            />
+                            <Checkbox checked=datatypepropertycheck label="Datatype properties" />
+                            <Checkbox checked=objectpropertycheck label="Object properties" />
+                            <Checkbox checked=solitarypropertycheck label="Solitary properties" />
+                            <Checkbox checked=classdisjointnesscheck label="Class disjointness" />
                             <Checkbox checked=setoperatorscheck label="Set operators" />
-                            <Slider
-                                value=degreeofcollapsingcheck
-                                max=16.0
-                                step=1.0
-                                show_stops=false
-                            />
+                            <Slider value=degreeofcollapsingcheck max=16.0 step=1.0 show_stops=false />
                             <Label>"Degree of collapsing"</Label>
                         </Menu>
 
-                        // options menu
+                        //options menu
                         <Menu on_select position=MenuPosition::Top>
                             <MenuTrigger slot>
                                 <Button icon=icondata::IoOptions>"Options"</Button>
                             </MenuTrigger>
                             <Checkbox checked=zoomcheck label="Zoom controls" />
-                            <Slider
-                                value=classdistancevalue
-                                max=600.0
-                                step=10.0
-                                show_stops=false
-                            />
+                            <Slider value=classdistancevalue max=600.0 step=10.0 show_stops=false />
                             <Label>"Class Distance"</Label>
-                            <Slider
-                                value=datatypedistancevalue
-                                max=600.0
-                                step=10.0
-                                show_stops=false
-                            />
+                            <Slider value=datatypedistancevalue max=600.0 step=10.0 show_stops=false />
                             <Label>"Datatype Distance"</Label>
-                            <Checkbox
-                                checked=dynamiclabelcheck
-                                label="Dynamic labels"
-                            />
-                            <Slider
-                                value=maxlabelwidthvalue
-                                max=600.0
-                                step=10.0
-                                show_stops=false
-                            />
+                            <Checkbox checked=dynamiclabelcheck label="Dynamic labels" />
+                            <Slider value=maxlabelwidthvalue max=600.0 step=10.0 show_stops=false />
                             <Label>"Max label width"</Label>
                             <Checkbox checked=nodescalingcheck label="Node scaling" />
-                            <Checkbox
-                                checked=compactnotationcheck
-                                label="Compact notation"
-                            />
-                            <Checkbox
-                                checked=colorexternalscheck
-                                label="Color externals"
-                            />
+                            <Checkbox checked=compactnotationcheck label= "Compact notation" />
+                            <Checkbox checked=colorexternalscheck label="Color externals" />
                         </Menu>
-                        // modes menu
+                        //modes menu
                         <Menu on_select position=MenuPosition::Top>
                             <MenuTrigger slot>
                                 <Button icon=icondata::AiStarOutlined>"Modes"</Button>
@@ -179,46 +133,42 @@ fn Home() -> impl IntoView {
                             </MenuTrigger>
                             <Button>"MIT License © 2014-2019"</Button>
                             <Caption1Strong>"WebVOWL Developers:"</Caption1Strong>
-                            <Caption1Strong>
-                                "Vincent Link, Steffen Lohmann, Eduard Marbach, Stefan Negru, Vitalis Wiens"
-                            </Caption1Strong>
+                            <Caption1Strong>"Vincent Link, Steffen Lohmann, Eduard Marbach, Stefan Negru, Vitalis Wiens"</Caption1Strong>
                         </Menu>
                     </ConfigProvider>
                 </div>
-                // right info bar
+                //right info bar
                 <div class="info-bar">
-                    <div class="ontology-title">
-                        {move || ontologytitle.get()}
-                    </div>
-                    <p>{{ move || ontologyiri.get() }}</p>
+                    <div class="ontology-title">{move || ontologytitle.get()}</div>
+                    <p>{{move || ontologyiri.get()}}</p>
                     <p>{move || ontologyversion.get()}</p>
                     <Caption1Strong>"Author(s): "</Caption1Strong>
-                    <Caption1Strong>
-                        {move || ontologyauthors.get()}
-                    </Caption1Strong>
+                    <Caption1Strong>{move || ontologyauthors.get()}</Caption1Strong>
                     <Caption1Strong>"Language(s): "</Caption1Strong>
                 </div>
                 <div class="accordion-header">
                     <Accordion collapsible=true multiple=true>
                         <AccordionItem value="description">
-                            <AccordionHeader slot>"Description"</AccordionHeader>
-                            <p id="description" class="info-section-content">
-                                "The Friend of a Friend (FOAF) RDF vocabulary, described using W3C RDF Schema and the Web Ontology Language."
-                            </p>
+                            <AccordionHeader slot>
+                                "Description"
+                            </AccordionHeader>
+                            <p id="description" class="info-section-content">"The Friend of a Friend (FOAF) RDF vocabulary, described using W3C RDF Schema and the Web Ontology Language."</p>
                         </AccordionItem>
                     </Accordion>
                     <Accordion collapsible=true multiple=true>
                         <AccordionItem value="setadata">
-                            <AccordionHeader slot>"Metadata"</AccordionHeader>
-                            <p id="metadata" class="info-section-content">
-                                "title: Friend of a Friend (FOAF) vocabulary
-                                Work on the GoodRelations ontology and related research and development has been partly supported by the Austrian BMVIT/FFG under the FIT-IT Semantic Systems project myOntology (grant no. 812515/9284), by a Young Researcher's Grant (Nachwuchsfoerderung 2005-2006) from the Leopold-Franzens-Universitaet Innsbruck, by the European Commission under the project SUPER (FP6-026850), and by the German Federal Ministry of Research (BMBF) by a grant under the KMU Innovativ program as part of the Intelligent Match project (FKZ 01IS10022B). The"
-                            </p>
+                                <AccordionHeader slot>
+                                    "Metadata"
+                                </AccordionHeader>
+                                <p id="metadata" class="info-section-content">"title: Friend of a Friend (FOAF) vocabulary
+                                Work on the GoodRelations ontology and related research and development has been partly supported by the Austrian BMVIT/FFG under the FIT-IT Semantic Systems project myOntology (grant no. 812515/9284), by a Young Researcher's Grant (Nachwuchsfoerderung 2005-2006) from the Leopold-Franzens-Universitaet Innsbruck, by the European Commission under the project SUPER (FP6-026850), and by the German Federal Ministry of Research (BMBF) by a grant under the KMU Innovativ program as part of the Intelligent Match project (FKZ 01IS10022B). The"</p>
                         </AccordionItem>
                     </Accordion>
                     <Accordion collapsible=true multiple=true>
                         <AccordionItem value="statistics">
-                            <AccordionHeader slot>"Statistics"</AccordionHeader>
+                            <AccordionHeader slot>
+                                "Statistics"
+                            </AccordionHeader>
                             <div class="info-section-content">
                                 <p>"Classes: "</p>
                                 <p>"Object Properties: "</p>
@@ -231,10 +181,10 @@ fn Home() -> impl IntoView {
                     </Accordion>
                     <Accordion collapsible=true multiple=true>
                         <AccordionItem value="selection details">
-                            <AccordionHeader slot>"Selection Details"</AccordionHeader>
-                            <p id="selection_details" class="info-section-content">
-                                "Select an element in the visualization."
-                            </p>
+                            <AccordionHeader slot>
+                                "Selection Details"
+                            </AccordionHeader>
+                            <p id="selection_details" class="info-section-content">"Select an element in the visualization."</p>
                         </AccordionItem>
                     </Accordion>
                 </div>
