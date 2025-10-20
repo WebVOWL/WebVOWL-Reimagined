38e8b4c9d21a37e2a5e4c8e2ac592b8fe09ef78c
manfedhod <mho20@student.aau.dk>
2025-10-20 09:35:05 +0000

    Rearrange menu module imports for clarity and consistency

--- src/components/menu.rs
+++ src/components/menu.rs
@@ -1,8 +1,8 @@
 pub mod ontology_menu;
-pub mod export_menu;
+pub mod search_menu;
 pub mod filter_menu;
+pub mod export_menu;
 pub mod options_menu;
 pub mod modes_menu;
 pub mod about_menu;
 pub mod side_bar;
-pub mod search_menu;
