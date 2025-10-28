600b8d8f4a5db0975d96d67f891b0837a31bdd43
LouiseWolff17 <UM21TY@student.aau.dk>
2025-10-28 09:59:29 +0000

    cookies without consent cookie

--- src/session_handler.rs
+++ src/session_handler.rs
@@ -1,23 +1,14 @@
+use actix_web::Error;
 use actix_session::Session;
-use actix_web::{Error, HttpMessage, HttpRequest, HttpResponse};
 
-pub async fn index(session: Session, req: HttpRequest) -> Result<&'static str, Error> {
-    let cookie_accepted = req
-        .cookie("accepted")
-        .map(|c| c.value() == "true")
-        .unwrap_or(false); // consent cookie 
-
-    if cookie_accepted {
-        // access the session state
-        if let Some(count) = session.get::<i32>("counter")? {
-            println!("SESSION value: {}", count);
-            // modify the session state
-            session.insert("counter", count + 1)?;
-        } else {
-            session.insert("counter", 1)?;
-        }
+fn index(session: Session) -> Result<&'static str, Error> {
+    // access the session state
+    if let Some(count) = session.get::<i32>("counter")? {
+        println!("SESSION value: {}", count);
+        // modify the session state
+        session.insert("counter", count + 1)?;
     } else {
-        println!("User didn't accept cookies.")
+        session.insert("counter", 1)?;
     }
 
     Ok("Welcome!")
