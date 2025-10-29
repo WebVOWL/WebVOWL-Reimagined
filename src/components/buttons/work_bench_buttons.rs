ccf78981c29b3f8fe854c173c0b2170a75ba5036
manfedhod <mho20@student.aau.dk>
2025-10-29 08:47:43 +0000

    small changes to button icons and positions.

--- src/components/buttons/work_bench_buttons.rs
+++ src/components/buttons/work_bench_buttons.rs
@@ -116,26 +116,6 @@ pub fn ExportButton() -> impl IntoView {
 }
 
 #[component]
-pub fn PauseButton() -> impl IntoView {
-    view! {     
-        <ConfigProvider>
-            <Button class="work-bench-button"
-            shape=ButtonShape::Square 
-            icon=icondata::AiPauseOutlined></Button>
-        </ConfigProvider>
-    }
-}
-
-#[component]
-pub fn ResetButton() -> impl IntoView {
-    view! {      
-        <ConfigProvider>
-            <Button class="work-bench-button" shape=ButtonShape::Square icon=icondata::VsDebugRestart></Button>
-        </ConfigProvider>
-    }
-}
-
-#[component]
 pub fn OptionsButton() -> impl IntoView {
     let ShowSearchMenu(show_search_menu) = use_context::<ShowSearchMenu>().expect("ShowSearchMenu should be provided");
     let ShowOntologyMenu(show_ontology_menu) = use_context::<ShowOntologyMenu>().expect("ShowOntologyMenu should be provided");
@@ -148,7 +128,7 @@ pub fn OptionsButton() -> impl IntoView {
             <Button
                 class="work-bench-button-bottom-1"
                 shape=ButtonShape::Square
-                icon=icondata::IoOptions
+                icon=icondata::IoSettingsSharp
                 on_click=move |_| {
                     let current_options_state = show_options_menu.get();
                     show_ontology_menu.update(|val| *val = false);
