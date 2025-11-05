7ea30978ffccfc6d182038b4a6c4483ebece9606
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-11-05 13:55:19 +0000

    Refactor settings menu

--- src/components/menu/workbench/options_menu.rs
+++ src/components/menu/workbench/options_menu.rs
@@ -1,10 +1,9 @@
+use super::{WorkBenchButton, WorkbenchMenuItems};
 use leptos::prelude::*;
 use thaw::*;
-use crate::pages::home::*;
 
 #[component]
-pub fn OptionsMenu() -> impl IntoView {
-    let ShowOptionsMenu(show_options_menu) = use_context::<ShowOptionsMenu>().expect("ShowOptionsMenu should be provided");
+pub fn SimulatorSettings() -> impl IntoView {
     let search_query = RwSignal::new(String::new());
     let class_distance_value = RwSignal::new(0.0);
     let datatype_distance_value = RwSignal::new(0.0);
@@ -13,30 +12,67 @@ pub fn OptionsMenu() -> impl IntoView {
     let node_scaling_check = RwSignal::new(false);
     let compact_notation_check = RwSignal::new(false);
     let color_externals_check = RwSignal::new(false);
-    view! {
-        <div class=move || {
-        if show_options_menu.get() {
-            "workbench-menu"
-        } else {
-            "workbench-menu menu-hidden"
-        }
-        }>
-            <div class="workbench-menu-header">
-                <h3>"Options"</h3>
-            </div>
-            <div class="workbench-menu-content">
-                <p class="workbench-menu-text">"Class Distance"</p>
-                <Slider class="workbench-slider" value=class_distance_value max=600.0 step=10.0 show_stops=false />
-                <p class="workbench-menu-text">"Datatype Distance"</p>
-                <Slider class="workbench-slider" value=datatype_distance_value max=600.0 step=10.0 show_stops=false />
-                <Checkbox class="workbench-checkbox" checked=dynamic_label_check label="Dynamic labels" />
-                <p class="workbench-menu-text">"Max label width"</p>
-                <Slider class="workbench-slider" value=label_width_value max=600.0 step=10.0 show_stops=false />
-                <Checkbox class="workbench-checkbox" checked=node_scaling_check label="Node scaling" />
-                <Checkbox class="workbench-checkbox" checked=compact_notation_check label="Compact notation" />
-                <Checkbox class="workbench-checkbox" checked=color_externals_check label="Color externals" />
 
-            </div>
-        </div>
+    view! {
+        <fieldset>
+            <legend>"Simulation (WIP)"</legend>
+            <Flex gap=FlexGap::Size(20) vertical=true>
+                <Field label="Class Distance">
+                    <Slider
+                        value=class_distance_value
+                        max=600.0
+                        step=10.0
+                        show_stops=false
+                    />
+                </Field>
+                <Field label="Datatype Distance">
+                    <Slider
+                        value=datatype_distance_value
+                        max=600.0
+                        step=10.0
+                        show_stops=false
+                    />
+                </Field>
+
+                <Checkbox checked=dynamic_label_check label="Dynamic labels" />
+                <Checkbox checked=node_scaling_check label="Node scaling" />
+                <Checkbox
+                    checked=compact_notation_check
+                    label="Compact notation"
+                />
+                <Checkbox
+                    checked=color_externals_check
+                    label="Color externals"
+                />
+                <Field label="Max label width">
+                    <Slider
+                        value=label_width_value
+                        max=600.0
+                        step=10.0
+                        show_stops=false
+                    />
+                </Field>
+            </Flex>
+        </fieldset>
+    }
+}
+
+#[component]
+pub fn OptionsMenu() -> impl IntoView {
+    view! {
+        <Popover
+            trigger_type=PopoverTriggerType::Click
+            position=PopoverPosition::RightEnd
+        >
+            <PopoverTrigger slot>
+                <WorkBenchButton
+                    text="Settings"
+                    icon=icondata::BiMenuRegular
+                ></WorkBenchButton>
+            </PopoverTrigger>
+            <WorkbenchMenuItems title="Settings">
+                <SimulatorSettings />
+            </WorkbenchMenuItems>
+        </Popover>
     }
 }
