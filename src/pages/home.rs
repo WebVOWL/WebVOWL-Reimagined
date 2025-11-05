20d0bc9777b89efa208bc600fd595740b4fb197e
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-11-05 13:58:27 +0000

    Move theme selection to workbench

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,6 +1,6 @@
 use crate::components::menu::right_side_bar::RightSidebar;
 use crate::components::menu::workbench::Workbench;
-use crate::components::theme::ThemeSelection;
+
 use leptos::prelude::*;
 use leptos_meta::*;
 use thaw::{ConfigProvider, Layout};
@@ -11,8 +11,6 @@ pub fn Home() -> impl IntoView {
         <Title text="WebVOWL Reimagined" />
         <main>
             <canvas id="canvas" />
-
-            <ThemeSelection />
             <RightSidebar />
             <Workbench />
         </main>
