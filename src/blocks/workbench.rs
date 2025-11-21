015555a69d896ea4d8ecbd64258725add2b28615
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-11-21 10:53:48 +0000

    Add all menus

--- src/blocks/workbench.rs
+++ src/blocks/workbench.rs
@@ -1,16 +1,16 @@
 mod about_menu;
 mod export_menu;
-// mod filter_menu;
-// mod modes_menu;
+mod filter_menu;
 mod ontology_menu;
 mod options_menu;
-// mod search_menu;
+mod search_menu;
 
 use crate::components::lists::{ListDetails, ListElement};
-use crate::components::menu::{mega_menu::MegaMenu, vertical_menu::VerticalMenu};
-use leptos::prelude::*;
-use options_menu::OptionsMenu;
+use crate::components::menu::vertical_menu::VerticalMenu;
 use about_menu::AboutMenu;
+use export_menu::ExportMenu;
+use filter_menu::FilterMenu;
+use leptos::prelude::*;
 use ontology_menu::OntologyMenu;
 use options_menu::OptionsMenu;
 use search_menu::SearchMenu;
@@ -18,13 +18,11 @@ use search_menu::SearchMenu;
 #[component]
 fn WorkbenchMenuItems(#[prop(into)] title: String, children: Children) -> impl IntoView {
     view! {
-        <div class="flex justify-center flex-col w-[250px] p-2">
+        <div class="flex flex-col justify-center p-2 w-[250px]">
             <div class="workbench-menu-header">
                 <h3>{title}</h3>
             </div>
-            <div>
-                {children()}
-            </div>
+            <div>{children()}</div>
         </div>
     }
 }
@@ -33,26 +31,20 @@ fn WorkbenchMenuItems(#[prop(into)] title: String, children: Children) -> impl I
 pub fn NewWorkbench() -> impl IntoView {
     view! {
         <VerticalMenu>
-            <ListElement
-                title="Load Ontology"
-                icon=icondata::BiMenuRegular
-            >
-                <OntologyMenu/>
+            <ListElement title="Load Ontology" icon=icondata::BiMenuRegular>
+                <OntologyMenu />
             </ListElement>
 
             <ListElement title="Search" icon=icondata::BiMenuRegular>
-                <button />
+                <SearchMenu />
             </ListElement>
 
             <ListElement title="Filter" icon=icondata::BiMenuRegular>
-                <button />
+                <FilterMenu />
             </ListElement>
 
-            <ListElement
-                title="Export"
-                icon=icondata::BiMenuRegular
-            >
-                <ExportMenu/>
+            <ListElement title="Export" icon=icondata::BiMenuRegular>
+                <ExportMenu />
             </ListElement>
 
             <ListDetails title="Settings" icon=icondata::IoSettingsOutline>
@@ -61,11 +53,8 @@ pub fn NewWorkbench() -> impl IntoView {
                 </ListElement>
             </ListDetails>
 
-            <ListElement
-                title="About"
-                icon=icondata::BiMenuRegular
-            >
-                <AboutMenu/>
+            <ListElement title="About" icon=icondata::BiMenuRegular>
+                <AboutMenu />
             </ListElement>
         </VerticalMenu>
     }
