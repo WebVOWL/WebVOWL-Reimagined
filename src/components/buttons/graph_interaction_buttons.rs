097ad6993f8761fcd74d65db52a46d582645a77b
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-11-21 09:57:33 +0000

    Use Icon instead of MaybeShowIcon

--- src/components/buttons/graph_interaction_buttons.rs
+++ src/components/buttons/graph_interaction_buttons.rs
@@ -1,7 +1,7 @@
+use crate::components::icon::Icon;
 use grapher::web::prelude::EVENT_DISPATCHER;
 use grapher::web::prelude::RenderEvent;
 use leptos::prelude::*;
-use crate::components::icon::MaybeShowIcon as Icon;
 
 #[component]
 pub fn PauseButton(#[prop(into)] is_sidebar_open: RwSignal<bool>) -> impl IntoView {
