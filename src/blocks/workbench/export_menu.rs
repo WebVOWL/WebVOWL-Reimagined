7083f3a3a550e20669cdfbbca3ca85cc3bf4f202
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-11-21 11:29:31 +0000

    Fix icon being import instead of export

--- src/blocks/workbench/export_menu.rs
+++ src/blocks/workbench/export_menu.rs
@@ -1,14 +1,12 @@
-use crate::components::icon::Icon;
 use super::WorkbenchMenuItems;
+use crate::components::icon::Icon;
 use leptos::prelude::*;
-
 
 #[component]
 pub fn ExportButton(
     #[prop(into)] label: String,
     #[prop(into)] icon: icondata::Icon,
-    #[prop(optional)] on_click: Option<Callback<()>>
-
+    #[prop(optional)] on_click: Option<Callback<()>>,
 ) -> impl IntoView {
     let onclick_handler = move |_| {
         if let Some(cb) = &on_click {
@@ -17,7 +15,7 @@ pub fn ExportButton(
     };
 
     view! {
-        <button 
+        <button
             class="relative flex items-center justify-center gap-1 h-10 w-40 m-2 bg-gray-200 text-[#000000] rounded-sm font-semibold"
             on:click=onclick_handler
         >
@@ -29,17 +27,16 @@ pub fn ExportButton(
 
 #[component]
 pub fn ExportMenu() -> impl IntoView {
-
     view! {
         <WorkbenchMenuItems title="Export Ontology">
             <div class="flex justify-center flex-wrap w-full">
-                <ExportButton label="Json" icon=icondata::AiDownloadOutlined />
-                <ExportButton label="SVG" icon=icondata::AiDownloadOutlined />
-                <ExportButton label="TeX" icon=icondata::AiDownloadOutlined />
-                <ExportButton label="TTL" icon=icondata::AiDownloadOutlined />
-                <ExportButton label="URL" icon=icondata::AiDownloadOutlined />
-                <ExportButton label="RDF" icon=icondata::AiDownloadOutlined />
-                <ExportButton label="OWL" icon=icondata::AiDownloadOutlined />
+                <ExportButton label="Json" icon=icondata::BiExportRegular />
+                <ExportButton label="SVG" icon=icondata::BiExportRegular />
+                <ExportButton label="TeX" icon=icondata::BiExportRegular />
+                <ExportButton label="TTL" icon=icondata::BiExportRegular />
+                <ExportButton label="URL" icon=icondata::BiExportRegular />
+                <ExportButton label="RDF" icon=icondata::BiExportRegular />
+                <ExportButton label="OWL" icon=icondata::BiExportRegular />
             </div>
         </WorkbenchMenuItems>
     }
