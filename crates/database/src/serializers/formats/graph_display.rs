624caac0629631396e514da74279db1818a41410
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-11-27 14:14:11 +0000

    Begin implementing IRI to Nodetype converter

--- crates/database/src/serializers/formats/graph_display.rs
+++ crates/database/src/serializers/formats/graph_display.rs
@@ -2,7 +2,17 @@ use grapher::web::prelude::NodeType;
 
 /// Struct containing graph data for RustGrapher
 pub struct GraphDisplayData {
-    labels: Vec<String>,
-    nodes: Vec<NodeType>,
-    edges: Vec<[usize; 3]>,
+    pub labels: Vec<String>,
+    pub nodes: Vec<NodeType>,
+    pub edges: Vec<[usize; 3]>,
+}
+
+impl GraphDisplayData {
+    pub fn new_empty() -> Self {
+        Self {
+            labels: Vec::new(),
+            nodes: Vec::new(),
+            edges: Vec::new(),
+        }
+    }
 }
--- crates/database/src/serializers/frontend.rs
+++ crates/database/src/serializers/frontend.rs
@@ -1,2 +1,44 @@
+use crate::serializers::formats::graph_display::GraphDisplayData;
+use fluent_uri::Iri;
+use grapher::web::prelude::NodeType;
+use memchr::{memchr, memchr2, memchr3};
+use rdf_fusion::model::{TermRef, VariableRef};
+
 // TODO: Use the structure of RDF Fusion's JSON serializer for this module
-pub struct FrontendSerializer {}
+pub struct GraphDisplayDataSolutionSerializer;
+
+impl GraphDisplayDataSolutionSerializer {
+    pub fn serialize<'a>(
+        buffer: &mut GraphDisplayData,
+        solution: impl IntoIterator<Item = (VariableRef<'a>, TermRef<'a>)>,
+    ) {
+        for (variable, value) in solution {
+            Self::write_term(buffer, value);
+        }
+    }
+
+    /// Edges
+    fn write_term<'a>(buffer: &mut GraphDisplayData, term: TermRef<'a>) {
+        match term {
+            TermRef::NamedNode(uri) => {
+                // TODO: Collect errors and show to frontend
+                let iri = Iri::parse(uri.as_str()).unwrap();
+            }
+            TermRef::BlankNode(bnode) => {}
+            TermRef::Literal(literal) => {}
+            #[cfg(feature = "sparql-12")]
+            TermRef::Triple(triple) => {}
+        }
+    }
+
+    // fn get_nodetype(iri: Iri<&str>) -> NodeType {
+    //     let path = iri.path();
+    //     let haystack = path.as_str().as_bytes();
+    //     let iri_types = ["rdf", "owl"];
+
+    //     let iri_type = memchr2("rdf", "owl", haystack);
+    // }
+
+    // // TODO: Make one for each supported type, e.g. RDF, RDDS, OWL, SKOS, etc
+    // fn try_get_rdf_nodetype(fragment: &str) -> Option<NodeType> {}
+}
