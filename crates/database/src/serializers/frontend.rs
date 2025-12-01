e6eea28b1e113c7fe558be0d46303d969542e13d
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-12-01 14:12:08 +0000

    Begin mapping nodes to ElementTypes

--- crates/database/src/serializers/frontend.rs
+++ crates/database/src/serializers/frontend.rs
@@ -1,6 +1,9 @@
 use crate::serializers::formats::graph_display::GraphDisplayData;
 use fluent_uri::Iri;
-// use grapher::web::prelude::ElementType;
+use grapher::web::prelude::{
+    ElementType, GenericNode, GenericType, RdfEdge, RdfNode, RdfType, RdfsEdge, RdfsNode, RdfsType,
+};
+use log::info;
 use rdf_fusion::model::{
     BlankNodeRef, TermRef, VariableRef,
     vocab::{rdf, rdfs, xsd},
@@ -18,6 +21,10 @@ impl GraphDisplayDataSolutionSerializer {
             Self::write_term(buffer, value);
         }
     }
+
+    // fn write_predicate<'a>(buffer: &mut GraphDisplayData, term: TermRef<'a>) {
+    //     match term {}
+    // }
 
     fn write_term<'a>(buffer: &mut GraphDisplayData, term: TermRef<'a>) {
         // TODO: Collect errors and show to frontend
@@ -40,40 +47,53 @@ impl GraphDisplayDataSolutionSerializer {
                 // NOTE: Only supports RDF 1.1
                 // TODO: Make a vocab for OWL
                 let element_type = match uri {
-                    rdf::ALT => {}
-                    rdf::BAG => {}
-                    rdf::FIRST => {}
-                    rdf::HTML => {}
-                    rdf::LANG_STRING => {}
-                    rdf::LIST => {}
-                    rdf::NIL => {}
-                    rdf::OBJECT => {}
-                    rdf::PREDICATE => {}
-                    rdf::PROPERTY => {}
-                    rdf::REST => {}
-                    rdf::SEQ => {}
-                    rdf::STATEMENT => {}
-                    rdf::SUBJECT => {}
-                    rdf::TYPE => {}
-                    rdf::VALUE => {}
-                    rdf::XML_LITERAL => {}
-                    rdfs::CLASS => {}
-                    rdfs::COMMENT => {}
-                    rdfs::CONTAINER => {}
-                    rdfs::CONTAINER_MEMBERSHIP_PROPERTY => {}
-                    rdfs::DATATYPE => {}
+                    // rdf::ALT => {}
+                    // rdf::BAG => {}
+                    // rdf::FIRST => {}
+                    // rdf::HTML => {}
+                    // rdf::LANG_STRING => {}
+                    // rdf::LIST => {}
+                    // rdf::NIL => {}
+                    // rdf::OBJECT => {}
+                    // rdf::PREDICATE => {}
+                    rdf::PROPERTY => buffer
+                        .nodes
+                        .push(ElementType::Rdf(RdfType::Edge(RdfEdge::RdfProperty))),
+                    // rdf::REST => {}
+                    // rdf::SEQ => {}
+                    // rdf::STATEMENT => {}
+                    // rdf::SUBJECT => {}
+                    // rdf::TYPE => {}
+                    // rdf::VALUE => {}
+                    // rdf::XML_LITERAL => {}
+                    rdfs::CLASS => {
+                        buffer
+                            .nodes
+                            .push(ElementType::Rdfs(RdfsType::Node(RdfsNode::Class)));
+                    }
+                    // rdfs::COMMENT => {}
+                    // rdfs::CONTAINER => {}
+                    // rdfs::CONTAINER_MEMBERSHIP_PROPERTY => {}
+                    rdfs::DATATYPE => buffer
+                        .nodes
+                        .push(ElementType::Rdfs(RdfsType::Edge(RdfsEdge::Datatype))),
                     rdfs::DOMAIN => {}
-                    rdfs::IS_DEFINED_BY => {}
+                    // rdfs::IS_DEFINED_BY => {}
                     rdfs::LABEL => {}
-                    rdfs::LITERAL => {}
-                    rdfs::MEMBER => {}
+                    rdfs::LITERAL => buffer
+                        .nodes
+                        .push(ElementType::Rdfs(RdfsType::Node(RdfsNode::Literal))),
+                    // rdfs::MEMBER => {}
                     rdfs::RANGE => {}
-                    rdfs::RESOURCE => {}
-                    rdfs::SEE_ALSO => {}
-                    rdfs::SUB_CLASS_OF => {}
+                    // rdfs::RESOURCE => {}
+                    // rdfs::SEE_ALSO => {}
+                    rdfs::SUB_CLASS_OF => buffer
+                        .nodes
+                        .push(ElementType::Rdfs(RdfsType::Edge(RdfsEdge::SubclassOf))),
                     rdfs::SUB_PROPERTY_OF => {}
                     _ => {
                         // Visualization of this element is not supported
+                        info!("Visualization of element '{}' is not supported", term)
                     }
                 };
             }
@@ -85,7 +105,12 @@ impl GraphDisplayDataSolutionSerializer {
                     // Named node
                 }
             }
-            TermRef::Literal(literal) => {}
+            TermRef::Literal(literal) => {
+                info!("Is literal: '{}'", literal.value())
+                // match literal {
+
+                // }
+            }
         }
     }
 
