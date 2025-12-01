f4872af23ba4c1ac263f8e7d3db9a5a873847223
LouiseWolff17 <UM21TY@student.aau.dk>
2025-12-01 10:23:11 +0000

    added server side functions for remote and SPARQL

--- src/blocks/workbench/ontology_menu.rs
+++ src/blocks/workbench/ontology_menu.rs
@@ -1,6 +1,8 @@
 use super::WorkbenchMenuItems;
 use leptos::prelude::*;
-use web_sys::{HtmlInputElement, FileList};
+use web_sys::{FormData, HtmlInputElement, FileList};
+use crate::components::user_input::file_upload::FileUpload;
+
 
 #[component]
 fn SelectStaticInput() -> impl IntoView {
@@ -41,13 +43,28 @@ fn SelectStaticInput() -> impl IntoView {
 }
 
 #[component]
-fn UploadInput() -> impl IntoView {
+fn UploadInput(upload: FileUpload) -> impl IntoView {
     let message = RwSignal::new(String::new());
     // TODO: This should be a loading widget until stuff has loaded
     // Maybe, if possible, also make some sort of progress bar or status update.
     let custom_request = move |file_list: FileList| {
         let len = file_list.length();
         message.set(format!("Number of uploaded files: {}", len));
+
+        let form = FormData::new().unwrap();
+        for i in 0..len {
+            if let Some(file) = file_list.item(i) {
+                form.append_with_blob("file_to_upload", &file).unwrap();
+            }
+        }
+
+        upload.local_action.dispatch_local(form);
+        upload.mode.set("local".into());
+    };
+
+    let handle_url = move |url: String| {
+        upload.remote_action.dispatch(url);
+        upload.mode.set("remote".into());
     };
 
     // TODO: Make accept formats a pointer to somewhere in network module as it should have definitions for accepted input.
@@ -57,8 +74,13 @@ fn UploadInput() -> impl IntoView {
             <input
                 class="w-full border-b-0 rounded p-1 bg-gray-200"
                 placeholder="Enter input URL"
+                on:change=move |ev| {
+                    let target: HtmlInputElement = event_target::<HtmlInputElement>(&ev);
+                    handle_url(target.value());
+                }
             />
         </div>
+
         <div class="mb-2">
             <label class="block mb-1">"From File:"</label>
             <div class="relative">
@@ -89,7 +111,11 @@ fn UploadInput() -> impl IntoView {
     }
 }
 
-fn Sparql() -> impl IntoView {
+
+#[component]
+fn Sparql(upload: FileUpload) -> impl IntoView {
+    let endpoint_signal = RwSignal::new(String::new());
+    let query_signal = RwSignal::new(String::new());
 
     let textarea_ref = NodeRef::<leptos::html::Textarea>::new();
 
@@ -104,13 +130,27 @@ fn Sparql() -> impl IntoView {
         }
     };
 
+    let run_sparql = move || {
+        upload.mode.set("sparql".into());
+        upload.sparql_action.dispatch((
+            endpoint_signal.get(),
+            query_signal.get(),
+            Some("json".to_string()),
+        ));
+    };
+
     view! {
         <fieldset>
             <legend>"SPARQL Query:"</legend>
             <div class="flex flex-col gap-2">
                 <div>
                     <label class="block text-xs text-gray mb-1">"Query Endpoint"</label>
-                    <input class="w-full text-xs border-b-0 rounded p-1 bg-gray-200" placeholder="Enter query endpoint"/>
+                    <input class="w-full text-xs border-b-0 rounded p-1 bg-gray-200" placeholder="Enter query endpoint"
+                        on:input=move |ev| {
+                            let t: HtmlInputElement = event_target(&ev);
+                            endpoint_signal.set(t.value());
+                        }
+                    />
                 </div>
 
                 <div>
@@ -119,11 +159,18 @@ fn Sparql() -> impl IntoView {
                         node_ref=textarea_ref
                         class="w-full text-xs border-b-0 rounded p-1 resize-none overflow-hidden min-h-24 bg-gray-200"
                         rows=1
-                        placeholder="Enter SPARQL query"
-                        on:input=handle_input
-                        
+                        placeholder="Enter query"
+                        on:input=move |ev| {
+                            let t: HtmlInputElement = event_target(&ev);
+                            query_signal.set(t.value());
+                            handle_input(());
+                        }
                     />
                 </div>
+
+                <button class="p-1 mt-1 rounded bg-blue-500 text-white text-xs"
+                        on:click=move |_| run_sparql()
+                >"Run query"</button>
             </div>
         </fieldset>
     }
@@ -131,11 +178,21 @@ fn Sparql() -> impl IntoView {
 
 #[component]
 pub fn OntologyMenu() -> impl IntoView {
+    let upload = FileUpload::new();
     view! {
         <WorkbenchMenuItems title="Load Ontology">
             <SelectStaticInput />
-            <UploadInput />
-            <Sparql />
+            <UploadInput upload=upload.clone()/>
+            <Sparql upload=upload.clone()/>
+
+            // TEST ONLY
+            // {
+            //     move || {
+            //         upload.get_result().map(|result| {
+            //             view! {<p class="text-xs mt-2">{format!("{:?}", result)}</p>}
+            //         })
+            //     }
+            // }
         </WorkbenchMenuItems>
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub use grapher::web::init_render;
 #[cfg(feature = "wasm")]
 pub use wasm_bindgen_rayon::init_thread_pool;
 
-#[cfg(feature = "server")]
+// #[cfg(feature = "server")]
 pub mod network;
 
 pub mod app;
