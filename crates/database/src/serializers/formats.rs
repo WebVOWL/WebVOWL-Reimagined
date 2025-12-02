c3249826a39d6f73f90222f8a06a23bdddffcd31
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-12-02 12:02:25 +0100

    Simplify formats file

--- crates/database/src/serializers/formats.rs
+++ crates/database/src/serializers/formats.rs
@@ -1,1 +1,28 @@
-pub mod graph_display;
+pub mod graph_display {
+    use grapher::web::prelude::ElementType;
+
+    /// Struct containing graph data for RustGrapher
+    pub struct GraphDisplayData {
+        /// Labels annotate classes and properties
+        pub labels: Vec<String>,
+        /// Elements are the nodes and edge types for which visualization is supported.
+        ///
+        /// The index into this vector determines the unique ID of each element
+        /// and is used by `edges`.
+        pub elements: Vec<ElementType>,
+        /// An array of three elements: `source node`, `edge`, and `target node`.
+        ///
+        /// The elements of the array are the indices of `elements`.
+        pub edges: Vec<[usize; 3]>,
+    }
+
+    impl GraphDisplayData {
+        pub fn new_empty() -> Self {
+            Self {
+                labels: Vec::new(),
+                elements: Vec::new(),
+                edges: Vec::new(),
+            }
+        }
+    }
+}
