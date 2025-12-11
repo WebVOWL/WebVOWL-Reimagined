0706627cb37830d0084f4bf6100dd80fd470828c
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-12-11 14:28:38 +0000

    Implement better debug logging

--- crates/database/src/main.rs
+++ crates/database/src/main.rs
@@ -1,25 +1,24 @@
 mod store;
-
+use env_logger::Env;
 use grapher::prelude::GraphDisplayData;
+use log::info;
 use rdf_fusion::{execution::results::QueryResults, store::Store};
 use std::path::Path;
-use webvowl_database::prelude::serializers::frontend::GraphDisplayDataSolutionSerializer;
+use webvowl_database::prelude::GraphDisplayDataSolutionSerializer;
 use webvowl_database::store::{DEFAULT_QUERY, WebVOWLStore};
 
 #[tokio::main]
 pub async fn main() {
-    //let session = Store::open("oxigraph.db").unwrap();
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
     let session = Store::default();
-    println!("Loaded {} quads", session.len().await.unwrap());
-    // let path = Path::new("crates/database/owl1-compatible.owl");
     let path = Path::new("crates/database/owl1-compatible.owl");
     let webvowl = WebVOWLStore::new(session);
     webvowl
         .insert_file(&path, false)
         .await
         .expect("Error inserting file");
-    println!("Loaded {} quads", webvowl.session.len().await.unwrap());
-    
+    info!("Loaded {} quads", webvowl.session.len().await.unwrap());
+
     let mut data_buffer = GraphDisplayData::new();
     let mut solution_serializer = GraphDisplayDataSolutionSerializer::new();
     let query_stream = webvowl.session.query(DEFAULT_QUERY).await.unwrap();
@@ -31,9 +30,10 @@ pub async fn main() {
     } else {
         panic!("Query stream is not a solutions stream");
     }
+    info!("--- GraphDisplayData ---");
     print_graph_display_data(&data_buffer);
-    println!("{}", solution_serializer);
-    println!("Written to Output.owl");
+    info!("--- SolutionSerializer ---");
+    info!("{}", solution_serializer);
 }
 
 pub fn print_graph_display_data(data_buffer: &GraphDisplayData) {
@@ -43,9 +43,9 @@ pub fn print_graph_display_data(data_buffer: &GraphDisplayData) {
         .zip(data_buffer.labels.iter())
         .enumerate()
     {
-        println!("{index}: {element:?} -> {label}");
+        info!("{index}: {element:?} -> {label}");
     }
     for edge in data_buffer.edges.iter() {
-        println!("{edge:?}");
+        info!("Edge: '{edge:?}'");
     }
 }
