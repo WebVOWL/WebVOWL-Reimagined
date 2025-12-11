8d5dd97f87cb43713fa812da16c7760d36d9eb3b
khmikkelsen <kh.mikkelsen@hotmail.com>
2025-12-11 01:36:28 +0100

    testing

--- crates/database/src/main.rs
+++ crates/database/src/main.rs
@@ -1,6 +1,5 @@
 mod store;
 
-use env_logger::Env;
 use grapher::prelude::GraphDisplayData;
 use rdf_fusion::{execution::results::QueryResults, store::Store};
 use std::path::Path;
@@ -9,7 +8,6 @@ use webvowl_database::store::{DEFAULT_QUERY, WebVOWLStore};
 
 #[tokio::main]
 pub async fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
     //let session = Store::open("oxigraph.db").unwrap();
     let session = Store::default();
     println!("Loaded {} quads", session.len().await.unwrap());
@@ -21,14 +19,7 @@ pub async fn main() {
         .await
         .expect("Error inserting file");
     println!("Loaded {} quads", webvowl.session.len().await.unwrap());
-    /*
-    let mut stream = parse_stream_to(webvowl.session.stream().await.unwrap(), ResourceType::OWL)
-        .await
-        .unwrap();
-    let mut out = vec![];
-    while let Some(result) = stream.next().await {
-        out.extend(result.unwrap());
-    }*/
+    
     let mut data_buffer = GraphDisplayData::new();
     let mut solution_serializer = GraphDisplayDataSolutionSerializer::new();
     let query_stream = webvowl.session.query(DEFAULT_QUERY).await.unwrap();
@@ -41,9 +32,7 @@ pub async fn main() {
         panic!("Query stream is not a solutions stream");
     }
     print_graph_display_data(&data_buffer);
-    //let mut serializer = NewSerializer::<String>::default();
-    //serializer.serialize(webvowl.session).await.unwrap();
-    //println!("{}", String::from_utf8_lossy(&out));
+    println!("{}", solution_serializer);
     println!("Written to Output.owl");
 }
 
@@ -55,5 +44,8 @@ pub fn print_graph_display_data(data_buffer: &GraphDisplayData) {
         .enumerate()
     {
         println!("{index}: {element:?} -> {label}");
+    }
+    for edge in data_buffer.edges.iter() {
+        println!("{edge:?}");
     }
 }
