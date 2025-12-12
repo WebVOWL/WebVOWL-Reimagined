e7910fbc963332e6bf7f8f6e5cade1f94d49e719
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-12-12 13:07:28 +0000

    Move SPARQL queries to their own files

--- src/sparql_queries.rs
+++ src/sparql_queries.rs
@@ -1,2 +1,2 @@
-pub const TESTING: &str = include_str!("sparql_queries/testing.rq");
+pub const TESTING: &str = include_str!("sparql_queries/testing2.rq");
 pub const DEFAULT: &str = include_str!("sparql_queries/default.rq");
