32b91d24d8d3b8105de0976d00ed046a59288f80
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-12-15 08:58:04 +0000

    Use default SPARQL query

--- src/blocks/workbench/ontology_menu.rs
+++ src/blocks/workbench/ontology_menu.rs
@@ -1,6 +1,6 @@
 use super::WorkbenchMenuItems;
 use crate::components::{icon::Icon, user_input::file_upload::*};
-use crate::sparql_queries::TESTING;
+use crate::sparql_queries::DEFAULT;
 use grapher::prelude::{EVENT_DISPATCHER, RenderEvent};
 use leptos::prelude::*;
 use leptos::task::spawn_local;
@@ -59,7 +59,7 @@ fn UploadInput() -> impl IntoView {
         if let Some(value) = loading_done.get() {
             match value {
                 Ok(_) => spawn_local(async {
-                    let output_result = handle_internal_sparql(TESTING.to_string()).await;
+                    let output_result = handle_internal_sparql(DEFAULT.to_string()).await;
                     match output_result {
                         Ok(graph_data) => {
                             let _ = EVENT_DISPATCHER
@@ -162,7 +162,7 @@ fn FetchData() -> impl IntoView {
             <button class="relative flex items-center justify-center p-1 mt-1 rounded text-xs bg-gray-200 text-[#000000]"
             on:click=move |_| {
                 spawn_local(async {
-                    let output_result = handle_internal_sparql(TESTING.to_string()).await;
+                    let output_result = handle_internal_sparql(DEFAULT.to_string()).await;
                     match output_result {
                         Ok(graph_data) => {
                             let _ = EVENT_DISPATCHER.rend_write_chan.send(RenderEvent::LoadGraph(graph_data));},
