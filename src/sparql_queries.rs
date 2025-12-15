c7891d536519b71965dba6514b5b66d97af3a36d
manfedhod <mho20@student.aau.dk>
2025-12-15 13:29:40 +0000

    Moved SPARQL queries for filter menu patterns into seperate file. Added general queries to the SPARQL query generator function.

--- src/sparql_queries.rs
+++ src/sparql_queries.rs
@@ -1,3 +1,5 @@
 pub const TESTING: &str = include_str!("sparql_queries/testing2.rq");
 pub const DEFAULT: &str = include_str!("sparql_queries/default.rq");
 pub mod filter_menu_patterns;
+pub mod default;
+pub mod general;
---
+++ src/sparql_queries/default.rs
@@ -0,0 +1,158 @@
+pub const NAMED_CLASS: &str = 
+            r#"{
+                ?id a owl:Class .
+                FILTER(isIRI(?id))
+                BIND(owl:Class AS ?nodeType)
+            }"#;
+
+pub const EXTERNAL_CLASS: &str = 
+            r#"{
+                # SPARQL query not defined
+                BIND(<http://example.org/nothing> AS ?id)
+                BIND(<http://example.org/nothing> AS ?nodeType)
+                BIND("" AS ?label)
+                FILTER(false)
+            }"#;
+
+pub const DEPRECATED_CLASS: &str = 
+            r#"{
+                ?id owl:deprecated ?target .
+                BIND(owl:deprecated AS ?nodeType)
+            }"#;
+
+pub const EQUIVALENT_CLASS: &str = 
+            r#"{
+                ?id owl:equivalentClass ?target
+                BIND(owl:equivalentClass AS ?nodeType)
+            }"#;
+
+pub const ANONYMOUS_CLASS: &str = 
+            r#"{
+                FILTER(!isIRI(?id))
+                BIND("blanknode" AS ?nodeType)
+            }"#;
+
+pub const THING: &str = 
+            r#"{
+                ?id a owl:Thing .
+                BIND(owl:Thing AS ?nodeType)
+            }"#;
+
+pub const RDFS_CLASS: &str = 
+            r#"{
+                ?id a rdfs:Class .
+                FILTER(?id != owl:Class)
+                BIND(owl:Class AS ?nodeType)
+            }"#;
+
+pub const RDFS_RESOURCE: &str = 
+            r#"{
+                ?id a rdfs:Resource .
+                BIND(rdfs:Resource AS ?nodeType)
+            }"#;
+
+pub const RDFS_LITERAL: &str = 
+            r#"{
+                ?id a rdfs:Literal .
+                FILTER(isLiteral(?id))
+                BIND(rdfs:Literal AS ?nodeType)
+            }"#;
+
+pub const INTERSECTION_OF: &str = 
+            r#"{
+                ?id owl:intersectionOf ?target .
+                BIND(owl:intersectionOf AS ?nodeType)
+            }"#;
+
+pub const UNION_OF: &str = 
+            r#"{
+                ?id owl:unionOf ?list .
+                BIND(owl:unionOf AS ?nodeType)
+            }"#;
+
+pub const COMPLEMENT: &str = 
+            r#"{
+                ?id owl:complementOf ?target .
+                BIND(owl:complementOf AS ?nodeType)
+            }"#;
+
+pub const DISJOINT_UNION: &str = 
+            r#"{
+                ?id owl:disjointUnionOf ?list .
+                BIND(owl:disjointUnionOf AS ?nodeType)
+            }"#;
+
+pub const OBJECTPROPERTY: &str = 
+            r#"{
+                # SPARQL query not defined
+                BIND(<http://example.org/nothing> AS ?id)
+                BIND(<http://example.org/nothing> AS ?nodeType)
+                BIND("" AS ?label)
+                FILTER(false)
+            }"#;
+
+pub const DATATYPEPROPERTY: &str = 
+            r#"{
+                # SPARQL query not defined
+                BIND(<http://example.org/nothing> AS ?id)
+                BIND(<http://example.org/nothing> AS ?nodeType)
+                BIND("" AS ?label)
+                FILTER(false)
+            }"#;
+
+pub const SUBCLASSOF: &str = 
+            r#"{
+                ?id rdfs:subClassOf ?target .
+                BIND(rdfs:subClassOf AS ?nodeType)
+            }"#;
+
+pub const INVERSEOF: &str = 
+            r#"{
+                # SPARQL query not defined
+                BIND(<http://example.org/nothing> AS ?id)
+                BIND(<http://example.org/nothing> AS ?nodeType)
+                BIND("" AS ?label)
+                FILTER(false)
+            }"#;
+
+pub const DISJOINTWITH: &str = 
+            r#"{
+                # SPARQL query not defined
+                BIND(<http://example.org/nothing> AS ?id)
+                BIND(<http://example.org/nothing> AS ?nodeType)
+                BIND("" AS ?label)
+                FILTER(false)
+            }"#;
+
+pub const RDFPROPERTY: &str = 
+            r#"{
+                # SPARQL query not defined
+                BIND(<http://example.org/nothing> AS ?id)
+                BIND(<http://example.org/nothing> AS ?nodeType)
+                BIND("" AS ?label)
+                FILTER(false)
+            }"#;
+
+pub const DEPRECATEDPROPERTY: &str = 
+            r#"{
+                ?id owl:deprecated ?target .
+                BIND(owl:deprecated AS ?nodeType)
+            }"#;
+
+pub const EXTERNALPROPERTY: &str = 
+            r#"{
+                # SPARQL query not defined
+                BIND(<http://example.org/nothing> AS ?id)
+                BIND(<http://example.org/nothing> AS ?nodeType)
+                BIND("" AS ?label)
+                FILTER(false)
+            }"#;
+
+pub const VALUESFROM: &str = 
+            r#"{
+                # SPARQL query not defined
+                BIND(<http://example.org/nothing> AS ?id)
+                BIND(<http://example.org/nothing> AS ?nodeType)
+                BIND("" AS ?label)
+                FILTER(false)
+            }"#;
