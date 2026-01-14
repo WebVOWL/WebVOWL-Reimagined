49430932ecee471af319581f8fb9493cd7a44eaf
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-01-14 17:56:45 +0100

    Remove deprecated code

--- src/sparql_queries.rs
+++
@@ -1,6 +0,0 @@
-pub const TESTING: &str = include_str!("sparql_queries/testing2.rq");
-pub const DEFAULT: &str = include_str!("sparql_queries/default.rq");
-pub mod default;
-pub mod default_query;
-pub mod filter_menu_patterns;
-pub mod general;
